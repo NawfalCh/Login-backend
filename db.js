fd386ac6c9f706fb7804cd8bada2530b4a892b90
Nawfal Cherif <nawfalsh@gmail.com>
2025-12-18 22:37:03 +0100

    update DB Config

--- db.js
+++ db.js
@@ -2,6 +2,9 @@
 require('dotenv').config(); // Lädt die .env Variablen
 const { Pool } = require('pg');
 
+// ODER einfacher: Wenn der Host NICHT localhost ist, brauchen wir SSL
+const needsSSL = process.env.DB_HOST !== 'localhost';
+
 // Erstellt einen Pool von Verbindungen (besser für Performance)
 const pool = new Pool({
   user: process.env.DB_USER,
@@ -9,9 +12,15 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
+
+  // HIER IST DIE ÄNDERUNG:
+  ssl: needsSSL ? { rejectUnauthorized: false } : false
 });
 
 // Wir exportieren eine query-Funktion, die wir überall nutzen können
 module.exports = {
   query: (text, params) => pool.query(text, params),
 };
+
+
+
